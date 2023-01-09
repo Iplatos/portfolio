08947d732f8416f30bcf169202a7b2c0fa1cfc3e
iplatos <platonoff9168@gmail.com>
2023-01-09 12:12:47 +0300

    fix: header

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import s from "./Nav.module.scss"
 import { Link, animateScroll as scroll } from "react-scroll";
-
+import LightSpeed from 'react-reveal/LightSpeed';
 const Nav = () => {
     return (
-        <div className={s.nav}>
-            <Link
-                activeClass = {s.active}
-                to="Main"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}>Main</Link>
-            <Link
-                activeClass = {s.active}
-                to="Skills"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}>Skills</Link>
-           <Link
-           activeClass = {s.active}
-           to="Projects"
-           spy={true}
-           smooth={true}
-           offset={-70}
-           duration={500}>Projects</Link>
-            <Link
-                activeClass = {s.active}
-                to="Contacts"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}>Contacts</Link>
+        <LightSpeed top>
+            <div className={s.nav}>
+                <Link
+                    activeClass={s.active}
+                    to="Main"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}>Main</Link>
+                <Link
+                    activeClass={s.active}
+                    to="Skills"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}>Skills</Link>
+                <Link
+                    activeClass={s.active}
+                    to="Projects"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}>Projects</Link>
+                <Link
+                    activeClass={s.active}
+                    to="Contacts"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}>Contacts</Link>
 
-        </div>
+            </div>
+        </LightSpeed>
     );
 };
 

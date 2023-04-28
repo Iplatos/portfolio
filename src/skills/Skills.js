2e0396c19aaeaf7f0593a0f554876dcf2c27bb3a
iplatos <platonoff9168@gmail.com>
2023-04-28 13:56:14 +0300

    feat: new skills added

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -5,15 +5,35 @@ import Skill from "../skill/Skill";
 import Title from "../common/Components/Title/Title";
 import JsPNGIcon from "./../assets/image/js.png"
 import ReactPNGIcon from "./../assets/image/react.png"
-import ReduxPNGIcon from "./../assets/image/redux-react-javascript-freecodecamp-npm-png-favpng-6F2x50visKuC0trBQ0952Cm1E_t.jpg"
+import ReduxPNGIcon
+    from "./../assets/image/redux-react-javascript-freecodecamp-npm-png-favpng-6F2x50visKuC0trBQ0952Cm1E_t.jpg"
 import CssPngIcon from "./../assets/image/css.png"
 import Rotate from 'react-reveal/Rotate';
 import LightSpeed from 'react-reveal/LightSpeed';
-
+import TypeScriptPNGIcon from "./../assets/image/Typescript.png"
+import HTMLPNGIcon from "./../assets/image/HTML.png"
+import PostmanPNGIcon from "./../assets/image/Postman.png"
+import GitPNGIcon from "./../assets/image/github.jpg"
 
 const Skills = () => {
     const CssIcon = {
         backgroundImage: `url(${CssPngIcon})`,
+
+    }
+    const PostmanIcon = {
+        backgroundImage: `url(${PostmanPNGIcon})`,
+
+    }
+    const GitHubIcon = {
+        backgroundImage: `url(${GitPNGIcon})`,
+
+    }
+    const HTLMIcon = {
+        backgroundImage: `url(${HTMLPNGIcon})`,
+
+    }
+    const TypeScriptIcon = {
+        backgroundImage: `url(${TypeScriptPNGIcon})`,
 
     }
     const ReactIcon = {
@@ -32,14 +52,21 @@ const Skills = () => {
                 <LightSpeed top><Title text={"My skills"}/></LightSpeed>
                 <div className={style.skills}>
                     <Rotate top left>
+                        <Skill title={"HTML"} style={HTLMIcon} discription={"описание не очень большое"}/>
                         <Skill title={"JS"} style={JsIcon} discription={"описание не очень большое"}/>
                         <Skill title={"CSS"} style={CssIcon}
+                               discription={"описание не очень большое,не очень большое"}/>
+                        <Skill title={"TypeScript"} style={TypeScriptIcon}
                                discription={"описание не очень большое,не очень большое"}/>
                         <Skill title={"REACT"} style={ReactIcon}
                                discription={"описание не очень большое. Очень не большое. большое но не очень. но не большое "}/>
                         <Skill title={"REDUX"} style={ReduxIcon}
                                discription={"описание не очень большое. Очень не большое. большое но не очень. но не большое "}/>
+                        <Skill title={"Postman"} style={PostmanIcon} discription={"описание не очень большое"}/>
+                        <Skill title={"Git"} style={GitHubIcon} discription={"описание не очень большое"}/>
+
                     </Rotate>
+
                 </div>
             </div>
 
